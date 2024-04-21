8debfed6d31c1dc67e6eaee42972dbefba0a4fea
Kang Seounguk <160799011+KangBaekGwa@users.noreply.github.com>
2024-04-22 07:50:22 +0900

    Add files via upload

---
+++ apigateway-service/apigateway-service/src/test/java/BaekGwa/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package BaekGwa.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
